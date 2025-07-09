30aff2a59e149fb2ae10f5de40a31bb26929da3b
Areebasyed07 <syedreeb3@gmail.com>
2025-07-09 10:29:17 +0300

    Add files via upload

---
+++ sql_injection_AREEBA.php
@@ -0,0 +1,31 @@
+<?<php
+// Connect to database
+$conn =new mysqli('localhost', 'root', '', 'simple_crud');
+
+$username =$_GET['username'];
+$password =$_GET['password'];
+
+// Vulnerable to SQL Injection
+$sql = "SELECT * FROM users WHERE name = '$username' AND password = '$password'"
+echo "<script>console.log(\"SQL Query: " . addslashes($sql) . "\";</script>";
+qli_query($conn, $sql);
+
+if (mysqli_num_rows($result) . 0) {
+    echo "<h3> Logged in successfully!</h3>";
+    echo "<h4>User Details:</h4>";
+
+    // Loop through all matching users
+    while ($user = mysqli_fetch_assoc($result)) {
+        echo "<ul>";
+        foreach ($user as $key => $value) {
+            echo "<li><strong>$key: </strong> $value</li> ";
+        }
+        echo "</ul><hr>";
+    }
+ } else {
+    echo "<h3> Invalid login!</h3>";
+ }
+
+?>
+
+</php>
